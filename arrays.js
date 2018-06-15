23a5d5027e7b15faa018f55f754df6e4fcbeb05d
john argote <argote.john@gmail.com>
2018-06-15 15:44:14 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,48 @@
+var chocolateBars = ['snickers','hundred grand','kitkat','skittles'];
+
+function addElementToBeginningOfArray() {
+  var cars = ['Toyota','Dodge','Mercedes'];
+  var moreCars = ['Porsche',...cars];
+  return moreCars;
+}
+
+function destructivelyAddElementToBeginningOfArray() {
+  var colors = ['Green','Blue','Red'];
+  return colors.unshift('Purple');
+}
+
+function addElementToEndOfArray() {
+  var shapes = ['Square','Circle','Triangle'];
+  var moreShapes = [...shapes,'Rectangle'];
+  return moreShapes;
+}
+
+function destructivelyAddElementToEndOfArray() {
+  var pets = ['Cat','Dog','Snake'];
+   return pets.push('Rabbit');
+}
+
+function accessElementInArray() {
+  var planets = ['Mars','Pluto','Earth'];
+  return planets[1];
+}
+
+function destrucivelyRemoveElementFromBeginningOfArray() {
+  var names = ['James','Kevin','Sandra'];
+  return names.shift();
+}
+
+function removeElementFromBeginningOfArray() {
+  var numbers = ['one','two','three'];
+  return numbers.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray() {
+  var states = ['Texas','North Dakota','Colorado'];
+  return states.pop();
+}
+
+function removeElementFromEndOfArray() {
+  var fruits = ['Apple','Banana','Mango'];
+  return fruits.slice(0,fruits.lenght-1);
+}
